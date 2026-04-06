da94f49280dc88516762d9b107ad727cfc43fef5
DiegoRNR <diego.negrinramirez@gmail.com>
2026-04-06 13:33:01 -0400

    replaced static light with interactive one

--- frontend/src/components/Scene.tsx
+++ frontend/src/components/Scene.tsx
@@ -3,6 +3,7 @@ import { OrbitControls, Environment } from "@react-three/drei";
 import { Greenhouse } from "../models/Greenhouse";
 import { Pot } from "../models/Pot";
 import { Chrizantemos } from "../models/Chrizantemos";
+import { GreenhouseLight } from './canvas/GreenhouseLight';
 
 export default function Scene() {
   return (
@@ -10,8 +11,7 @@ export default function Scene() {
       camera={{ position: [1, 1.5, 2], fov: 45 }}
       className="w-full h-full"
     >
-      <ambientLight intensity={0.5} />
-      <directionalLight position={[0, 1, 0]} intensity={5} />
+      <GreenhouseLight />
       <Greenhouse />
       <Pot />
       <Chrizantemos />
