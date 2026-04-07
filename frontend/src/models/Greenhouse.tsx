18c5360bb26d10ed70cd0f7081162ad1e3f3386c
DiegoRNR <diego.negrinramirez@gmail.com>
2026-04-06 21:37:15 -0400

    changed to preload and primitive to all models

--- frontend/src/models/Greenhouse.tsx
+++ frontend/src/models/Greenhouse.tsx
@@ -4,3 +4,5 @@ export function Greenhouse() {
   const { scene } = useGLTF("/3d-models/greenhouse.glb");
   return <primitive object={scene} scale={1} position={[0.1, -0.2, 0]} />;
 }
+
+useGLTF.preload("/3d-models/greenhouse.glb");
--- frontend/src/models/plants/Chrizantemos.tsx
+++ frontend/src/models/plants/Chrizantemos.tsx
@@ -1,8 +1,8 @@
-import { Clone, useGLTF } from "@react-three/drei";
+import { useGLTF } from "@react-three/drei";
 
 export function Chrizantemos() {
   const { scene } = useGLTF("/3d-models/blue_chrizantemos.glb");
-  return <Clone object={scene} scale={0.4} position={[0.01, 0.03, -0.01]} />;
+  return <primitive object={scene} scale={0.4} position={[0.01, 0.03, -0.01]} />;
 }
 
 useGLTF.preload("/3d-models/blue_chrizantemos.glb");
