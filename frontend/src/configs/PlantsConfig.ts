3fc211959d725e02e42bffc98a665a812d3e085e
DiegoRNR <diego.negrinramirez@gmail.com>
2026-04-06 20:54:09 -0400

    added data interfaces and default configs for sensors and plants

---
+++ frontend/src/configs/PlantsConfig.ts
@@ -0,0 +1,23 @@
+import type { PlantData } from '../interfaces/PlantData';
+
+export const PLANTS: PlantData[] = [
+  { 
+    name: 'mint',
+    label: 'Mint', 
+    emoji: '🍃'
+  },
+  { 
+    name: 'thyme', 
+    label: 'Thyme', 
+    emoji: '🌱' 
+  },
+  { name: 'basil', 
+    label: 'Basil', 
+    emoji: '🌿' 
+  },
+  { 
+    name: 'rosemary', 
+    label: 'Rosemary', 
+    emoji: '🪴' 
+  },
+];
---
+++ frontend/src/interfaces/PlantData.ts
@@ -0,0 +1,7 @@
+type Plant = 'basil' | 'rosemary' | 'mint' | 'thyme'
+
+export interface PlantData {
+  name: Plant;
+  label: string;
+  emoji: string;
+}
