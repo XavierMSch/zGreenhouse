048041ca68e1e7debfdec443d4d5fe94239cec72
DiegoRNR <diego.negrinramirez@gmail.com>
2026-04-06 21:56:21 -0400

    rename model

--- frontend/src/models/GreenhouseFrame.tsx
+++ frontend/src/models/GreenhouseFrame.tsx
@@ -1,6 +1,6 @@
 import { useGLTF } from "@react-three/drei";
 
-export function Greenhouse() {
+export function GreenhouseFrame() {
   const { scene } = useGLTF("/3d-models/greenhouse.glb");
   return <primitive object={scene} scale={1} position={[0.1, -0.2, 0]} />;
 }
