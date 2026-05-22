1186cf331d493acc34fa8037f4b576ea41d121e5
Xavier M. S. <javier.schlechter@gmail.com>
2026-05-22 18:53:48 -0400

    Código de esp32 y gateway

---
+++ esp32/main.py
@@ -0,0 +1,29 @@
+import machine
+import dht
+import time
+import json
+import sys
+
+DHT_PIN = 15
+
+INTERVALO_LECTURA = 30
+
+sensor = dht.DHT22(machine.Pin(DHT_PIN))
+
+def send(obj):
+    print(json.dumps(obj))
+
+
+send({"status": "listo"})
+
+while True:
+    try:
+        sensor.measure()
+        send({
+            "temperatura": sensor.temperature(),
+            "humedad": sensor.humidity()
+        })
+    except Exception as e:
+        send({"error": str(e)})
+
+    time.sleep(INTERVALO_LECTURA)
